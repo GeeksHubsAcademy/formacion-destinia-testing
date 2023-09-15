1f22aed4bebad2a770d13f381c4c0deb2ab22c68
Jupegarnica <juan@geekshubs.com>
2023-09-15 12:52:09 +0200

    🔧 chore(MockingTest.php): remove unnecessary blank line 🔧 chore(MockingTest.php): add missing blank line for better readability ✨ feat(MockingTest.php): add test case for spying on a mock object's method and asserting its behavior

--- phpunit/complex/tests/MockingTest.php
+++ phpunit/complex/tests/MockingTest.php
@@ -3,7 +3,6 @@
 
 use PHPUnit\Framework\TestCase;
 
-
 class ClassToBeMocked
 {
     public function methodToBeMockedA()
@@ -24,6 +23,16 @@ class MockingTest extends TestCase
         $sut->method('methodToBeMockedA')->willReturn('C');
         $this->assertEquals('C', $sut->methodToBeMockedA());
         $this->assertEquals(null, $sut->methodToBeMockedB());
+    }
+
+    public function testSpyMock() {
+        $sut = $this->createMock(ClassToBeMocked::class);
+
+        $sut->expects($this->exactly(2))
+            ->method('methodToBeMockedA')
+            ->willReturn('C');
+        $this->assertEquals('C', $sut->methodToBeMockedA());
+        $sut->methodToBeMockedA();
     }
 
 }
