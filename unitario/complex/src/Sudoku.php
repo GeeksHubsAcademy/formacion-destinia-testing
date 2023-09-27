4f1b7dba4510b2350bb08897bdcce2d572ece331
Jupegarnica <juan@geekshubs.com>
2023-09-27 17:37:14 +0200

    🔨 refactor(makefile): update test-watch command to filter by Sudoku instead of MockingTest 🔨 refactor(Sudoku.php): improve code readability by adding spaces between elements in $expected array 🔨 refactor(Sudoku.php): rename isSolvedGeneric method to isValidGeneric for better semantics 🔨 refactor(Sudoku.php): rename isColumnSolved method to isColumnValid for better semantics 🔨 refactor(Sudoku.php): rename isSubgridSolved method to isSubgridValid for better semantics 🔨 refactor(Sudoku.php): rename isSolved method to isSolved for better semantics 🔨 refactor(Sudoku.php): add isValid method to check if the Sudoku is valid 🔨 refactor(Sudoku.php): extract common logic into isValidGeneric method for code reuse 🔨 refactor(Sudoku.php): rename isRowSolved method to isRowValid for better semantics 🔨 refactor(Sudoku.php): rename isSubgridSolved method to isSubGridValid for better semantics 🔨 refactor(Sudoku.php): extract getColumn method to get the values of a specific column 🔨 refactor(Sudoku.php): extract getSubgrid method to get the values of a specific subgrid 🔨 refactor(Sudoku.php): add getRestValues method to get the remaining values in an array
    🔧 chore(SudokuTest.php): add additional test cases to improve test coverage for the Sudoku class
    
    🔧 fix(SudokuTest.php): replace commented code with a dataProvider to improve readability and maintainability
    ✨ feat(SudokuTest.php): add test cases for different Sudoku grid scenarios to improve test coverage and ensure correctness of the Sudoku solver
    
    🔨 refactor(Sudoku.php): remove commented out code and unused test methods to improve code readability and maintainability
    🔧 chore(Sudoku.php): clean up whitespace and formatting for better code organization and readability
    🔬 test(SudokuTest.php): refactor testIsValid and testGetRestValues data providers to improve readability and maintainability

--- unitario/complex/src/Sudoku.php
+++ unitario/complex/src/Sudoku.php
@@ -10,9 +10,10 @@ class Sudoku
         $this->rows = $rows;
     }
 
-    public function isSolvedGeneric($array) {
+    public function isSolvedGeneric($array)
+    {
         sort($array);
-        $expected = [1,2,3,4,5,6,7,8,9];
+        $expected = [1, 2, 3, 4, 5, 6, 7, 8, 9];
         return $array == $expected;
     }
 
@@ -21,41 +22,27 @@ class Sudoku
 
         $currentRow = $this->rows[$row];
         return $this->isSolvedGeneric($currentRow);
-
-
     }
 
-    public function isColumnSolved($column) {
-        $currentColumn = [];
-        foreach ($this->rows as $row) {
-            $currentColumn[] = $row[$column];
-        }
-
+    public function isColumnSolved($column)
+    {
+        $currentColumn = $this->getColumn($column);
         return $this->isSolvedGeneric($currentColumn);
     }
 
-    public function isSubgridSolved($index) {
+    public function isSubgridSolved($index)
+    {
         // index 0 is first subgrid 3 x 3 top left
         // index 1 is second subgrid 3 x 3 top middle
         // ...
 
-        $currentSubgrid = [];
-        $rowIndex = floor($index / 3) * 3;
-        $columnIndex = ($index % 3) * 3;
-
-        for ($i = $rowIndex; $i < $rowIndex + 3; $i++) {
-            for ($j = $columnIndex; $j < $columnIndex + 3; $j++) {
-                $currentSubgrid[] = $this->rows[$i][$j];
-            }
-        }
+        $currentSubgrid = $this->getSubgrid($index);
 
         return $this->isSolvedGeneric($currentSubgrid);
-
-
-
     }
 
-    public function isSolved() {
+    public function isSolved()
+    {
         for ($i = 0; $i < 9; $i++) {
             if (!$this->isRowSolved($i)) {
                 return false;
@@ -69,5 +56,93 @@ class Sudoku
         }
 
         return true;
+    }
+    public function isValid()
+    {
+        for ($i = 0; $i < 9; $i++) {
+            if (!$this->isRowValid($i)) {
+                return false;
+            }
+            if (!$this->isColumnValid($i)) {
+                return false;
+            }
+            if (!$this->isSubgridValid($i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    private function isValidGeneric($array)
+    {
+        if (count($array) != 9) {
+            return false;
+        }
+        $numbers = [];
+        foreach ($array as $value) {
+            if ($value === 0) {
+                continue;
+            }
+            if (isset($numbers[$value])) {
+                return false;
+            }
+
+            if ($value >= 1 && $value <= 9) {
+                $numbers[$value] = true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private function isRowValid($row)
+    {
+        $currentRow = $this->rows[$row];
+
+        return $this->isValidGeneric($currentRow);
+    }
+    private function isColumnValid($column)
+    {
+        $currentColumn = $this->getColumn($column);
+        return $this->isValidGeneric($currentColumn);
+    }
+
+    private function isSubGridValid($index)
+    {
+        $currentSubgrid = $this->getSubgrid($index);
+
+        return $this->isValidGeneric($currentSubgrid);
+    }
+    private function getColumn($column)
+    {
+        $currentColumn = [];
+        foreach ($this->rows as $row) {
+            $currentColumn[] = $row[$column];
+        }
+        return $currentColumn;
+    }
+
+    private function getSubgrid($index)
+    {
+        $currentSubgrid = [];
+        $rowIndex = floor($index / 3) * 3;
+        $columnIndex = ($index % 3) * 3;
+
+        for ($i = $rowIndex; $i < $rowIndex + 3; $i++) {
+            for ($j = $columnIndex; $j < $columnIndex + 3; $j++) {
+                $currentSubgrid[] = $this->rows[$i][$j];
+            }
+        }
+        return $currentSubgrid;
+    }
+    public function getRestValues($array) {
+        $restValues = [];
+        for ($i = 1; $i <= 9; $i++) {
+            if (!in_array($i, $array)) {
+                $restValues[] = $i;
+            }
+        }
+        return $restValues;
     }
 }
