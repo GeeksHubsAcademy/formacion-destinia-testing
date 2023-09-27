5b200820d4f15d0941962c548fc5677a45a1c072
Jupegarnica <juan@geekshubs.com>
2023-09-27 16:13:49 +0200

    🔨 chore(makefile): update test-watch command in makefile to use MockingTest instead of SudokuTest 🔨 chore(MockingTest.php): add new test cases for methodToBeTested and StaticClass 🔨 chore(MockingTest.php): comment out testStaticMocked method for now

--- unitario/complex/tests/MockingTest.php
+++ unitario/complex/tests/MockingTest.php
@@ -15,8 +15,76 @@ class ClassToBeMocked
     }
 }
 
+class ServiceClass {
+    private $classToBeMocked;
+
+    public function __construct(ClassToBeMocked $classToBeMocked)
+    {
+        $this->classToBeMocked = $classToBeMocked;
+    }
+    public function methodToBeTested() {
+
+        return $this->$classToBeMocked->methodToBeMockedA();
+    }
+}
+
+
+class StaticClass {
+    static $state = 'A';
+
+    public function setState($data) {
+        self::$state = $data;
+    }
+    public function getState() {
+        return self::$state;
+    }
+}
+
 class MockingTest extends TestCase
 {
+
+
+    public function testWillReturnCB() {
+
+
+
+        $mock = $this->createMock(ClassToBeMocked::class);
+        $mock->method('methodToBeMockedA')->willReturnCallback(function() {
+            return 'CB';
+        });
+
+        $this->assertEquals('CB', $mock->methodToBeMockedA());
+
+    }
+
+    public function testStatic()
+    {
+
+        $instanceB = new StaticClass();
+
+        $instanceB->setState('B');
+
+        $this->assertEquals('B', $instanceB->getState());
+        $this->assertEquals('B', StaticClass::$state);
+
+
+        $instanceC = new StaticClass();
+        $this->assertEquals('B', $instanceC->getState());
+
+    }
+
+    // public function testStaticMocked() {
+
+    //     $mock = $this->createMock(StaticClass::class);
+    //     $mock->method('getState')->willReturn('C');
+
+
+
+
+    // }
+
+
+
     public function testMock()
     {
         $mock = $this->createMock(ClassToBeMocked::class);
