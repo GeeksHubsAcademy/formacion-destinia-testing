17378e203514205dae51100ab235c78dfdf766a4
Jupegarnica <juan@geekshubs.com>
2024-01-18 14:37:36 +0100

    sudoku tests passing

--- refactors/sudoku/cypress/e2e/sudoku.cy.js
+++ refactors/sudoku/cypress/e2e/sudoku.cy.js
@@ -1,12 +1,166 @@
 import Sudoku from "../../src/sudoku"
 
 describe('Sudoku', () => {
-  it('should validate a empty sudoku');
-  it('should validate a solved sudoku');
-  it('should validate a incomplete sudoku');
 
-  it('should validate a sudoku with a invalid row');
-  it('should validate a sudoku with a invalid column');
-  it('should validate a sudoku with a invalid block');
+  describe('set', () => {
+    it('should validate the size', () => {
+      const sudoku = new Sudoku();
+      expect(() => {
+        sudoku.set([[1]])
+      }).to.throw('Invalid size');
+
+    });
+    it('should validate the content negative numbers', () => {
+      const sudoku = new Sudoku();
+      expect(() => {
+        sudoku.set([
+          [-1, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ])
+      }).to.throw('Invalid negative number');
+    });
+
+    it('should validate the content float number', () => {
+      const sudoku = new Sudoku();
+      expect(() => {
+        sudoku.set([
+          [1.1, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ])
+      }).to.throw('Invalid float number');
+    });
+    it('should validate the content not numbers', () => {
+      const sudoku = new Sudoku();
+      expect(() => {
+        sudoku.set([
+          ['1', 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+          [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        ])
+      }).to.throw('Invalid not a number');
+    });
+  });
+
+  describe('isValid', () => {
+    it('should validate a empty sudoku', () => {
+      const sudoku = new Sudoku();
+      const emptySudoku = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      sudoku.set(emptySudoku);
+      expect(sudoku.isValid()).to.be.true;
+
+    });
+    it('should validate a solved sudoku', () => {
+      const sudoku = new Sudoku();
+      const solvedSudoku = [
+        [1, 2, 3, 4, 5, 6, 7, 8, 9,],
+        [7, 8, 9, 1, 2, 3, 4, 5, 6,],
+        [4, 5, 6, 7, 8, 9, 1, 2, 3,],
+        [3, 1, 2, 8, 4, 5, 9, 6, 7,],
+        [6, 9, 7, 3, 1, 2, 8, 4, 5,],
+        [8, 4, 5, 6, 9, 7, 3, 1, 2,],
+        [2, 3, 1, 5, 7, 4, 6, 9, 8,],
+        [9, 6, 8, 2, 3, 1, 5, 7, 4,],
+        [5, 7, 4, 9, 6, 8, 2, 3, 1,]
+      ];
+      sudoku.set(solvedSudoku);
+      expect(sudoku.isValid()).to.be.true;
+
+    });
+    it('should validate a incomplete sudoku', () => {
+      const sudoku = new Sudoku();
+      const solvedSudoku = [
+        [1, 2, 0, 4, 5, 6, 7, 8, 9,],
+        [7, 8, 9, 1, 2, 3, 4, 5, 6,],
+        [4, 5, 6, 0, 0, 9, 1, 2, 3,],
+        [3, 1, 2, 8, 4, 5, 9, 6, 7,],
+        [6, 9, 7, 3, 1, 2, 8, 4, 5,],
+        [8, 4, 5, 6, 9, 7, 3, 1, 2,],
+        [2, 3, 1, 5, 7, 4, 6, 9, 8,],
+        [9, 6, 8, 2, 3, 1, 0, 7, 4,],
+        [5, 7, 4, 9, 6, 8, 2, 0, 1,]
+      ];
+      sudoku.set(solvedSudoku);
+      expect(sudoku.isValid()).to.be.true;
+    });
+
+    it('should validate a sudoku with a invalid row', () => {
+      const sudoku = new Sudoku();
+      const solvedSudoku = [
+        [0, 0, 0, 4, 4, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      sudoku.set(solvedSudoku);
+      expect(sudoku.isValid()).to.be.false;
+    });
+    it('should validate a sudoku with a invalid column', () => {
+      const sudoku = new Sudoku();
+      const solvedSudoku = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [4, 0, 0, 0, 0, 0, 0, 0, 0],
+        [4, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      sudoku.set(solvedSudoku);
+      expect(sudoku.isValid()).to.be.false;
+    });
+    it('should validate a sudoku with a invalid block', () => {
+      const sudoku = new Sudoku();
+      const solvedSudoku = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 4, 0, 0, 0, 0, 0, 0, 0],
+        [4, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ];
+      sudoku.set(solvedSudoku);
+      expect(sudoku.isValid()).to.be.false;
+    });
+  });
 
 })
--- refactors/sudoku/src/sudoku.js
+++ refactors/sudoku/src/sudoku.js
@@ -1,4 +1,81 @@
-class Sudoku  {
+class Sudoku {
+    #grid;
+    set(grid) {
+        if (grid.length !== 9) {
+            throw new Error('Invalid size');
+        }
+        for (let i = 0; i < grid.length; i++) {
+            if (grid[i].length !== 9) {
+                throw new Error('Invalid size');
+            }
+            for (let j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] < 0) {
+                    throw new Error('Invalid negative number');
+                }
+                if (grid[i][j] % 1 !== 0) {
+                    throw new Error('Invalid float number');
+                }
+                if (typeof grid[i][j] !== 'number') {
+                    throw new Error('Invalid not a number');
+                }
+            }
+        }
+        this.#grid = grid;
+    }
+    isValid() {
+        const grid = this.#grid;
+        // check rows
+        for (let row = 0; row < grid.length; row++) {
+            const numbersFound = new Set();
+            for (let col = 0; col < grid[row].length; col++) {
+                const number = grid[row][col];
+                if (number === 0) {
+                    continue;
+                }
+                if (numbersFound.has(number)) {
+                    return false;
+                }
+                numbersFound.add(number);
+            }
+
+        }
+
+        // check cols
+        for (let col = 0; col < grid.length; col++) {
+            const numbersFound = new Set();
+            for (let row = 0; row < grid[col].length; row++) {
+                const number = grid[row][col];
+                if (number === 0) {
+                    continue;
+                }
+                if (numbersFound.has(number)) {
+                    return false;
+                }
+                numbersFound.add(number);
+            }
+
+        }
+
+        // check blocks 3x3
+        for (let row = 0; row < grid.length; row += 3) {
+            for (let col = 0; col < grid[row].length; col += 3) {
+                const numbersFound = new Set();
+                for (let i = row; i < row + 3; i++) {
+                    for (let j = col; j < col + 3; j++) {
+                        const number = grid[i][j];
+                        if (number === 0) {
+                            continue;
+                        }
+                        if (numbersFound.has(number)) {
+                            return false;
+                        }
+                        numbersFound.add(number);
+                    }
+                }
+            }
+        }
+        return true;
+    }
 
 }
 
